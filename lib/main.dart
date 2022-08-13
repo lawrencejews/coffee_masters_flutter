e763e430f0d9cb83ea220dd2fdc5cf2cf779e136
lawrencejews <lawrencejews1@gmail.com>
2022-08-13 23:21:00 +0900

    Enviromnent settings

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+// class HelloWorld extends StatelessWidget {
+//   const HelloWorld({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Text("HelloWorld");
+//   }
+// }
+
+class Greet extends StatefulWidget {
+  const Greet({Key? key}) : super(key: key);
+
+  @override
+  State<Greet> createState() => _GreetState();
+}
+
+// Private class widget
+class _GreetState extends State<Greet> {
+  // State variable
+  var name = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text("Hello $name"),
+        TextField(
+            onChanged: (value) => setState(() {
+                  name = value;
+                })),
+      ],
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+// HomePage Widgets
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Greet(),
+    );
+  }
+}
